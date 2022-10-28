2ab291efd27a4793fc583febed5867d3858b56cc
Adhitya Dharmawan Santosa <adhityadharmawan@gmail.com>
2022-10-28 20:46:20 +0700

    Day 25

--- src/main/java/com/juaracoding/cucumber/pages/RecruitmentPage.java
+++ src/main/java/com/juaracoding/cucumber/pages/RecruitmentPage.java
@@ -1,6 +1,7 @@
 package com.juaracoding.cucumber.pages;
 
 import com.juaracoding.cucumber.drivers.DriverSingleton;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -39,7 +40,7 @@ public class RecruitmentPage {
     }
 
     public void searchByHiringManager(){
-        selectHiringManager.sendKeys("L");
+        selectHiringManager.sendKeys("Li");
     }
 
     public void btnSearchByHiringManager(){
